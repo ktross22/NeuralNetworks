66d811bef68e1e56c3f6b95c7572034e365ebf90
олег поречнев <oleg.ktross@yandex.ru>
2020-10-30 08:43:55 +0300

    Нормализация и масштабирование данных

--- NeuralNetworks/NeuralNetwork.cs
+++ NeuralNetworks/NeuralNetwork.cs
@@ -26,6 +26,7 @@ namespace NeuralNetworks
 
         public Neuron Predict(params double[] inputSignals)
         {
+
             SendSignalsToInputNeurons(inputSignals);
             FeedForwardAllLayersAfterInput();
 
@@ -41,13 +42,15 @@ namespace NeuralNetworks
 
         public double Learn(double[] expected, double[,] inputs, int epoch)
         {
+            var signals = Normalization(inputs);
+
             var error = 0.0;
             for (int i = 0; i < epoch; i++)
             {
                 for (int j = 0; j < expected.Length; j++)
                 {
                     var output = expected[j];
-                    var input = GetRow(inputs, j);
+                    var input = GetRow(signals, j);
 
                     error += Backpropagation(output, input);
                 }
--- NeuralNetworksTests/NeuralNetworkTests.cs
+++ NeuralNetworksTests/NeuralNetworkTests.cs
@@ -2,6 +2,7 @@
 using NeuralNetworks;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -64,5 +65,52 @@ namespace NeuralNetworks.Tests
                 Assert.AreEqual(expected, actual);
             }
         }
+
+        [TestMethod()]
+        public void DatasetTest()
+        {
+            var outputs = new List<double>();
+            var inputs = new List<double[]>();
+            using (var sr = new StreamReader("heart.csv"))
+            {
+                var header = sr.ReadLine();
+                while(!sr.EndOfStream)
+                {
+                    var row = sr.ReadLine();
+                    var values = row.Split(',').Select(v => Convert.ToDouble(v.Replace(".", ","))).ToList();
+                    var output = values.Last();
+                    var input = values.Take(values.Count - 1).ToArray();
+
+                    outputs.Add(output);
+                    inputs.Add(input);
+                }
+            }
+
+            var inputSignals = new double[inputs.Count, inputs[0].Length];
+                for(int i =0; i< inputSignals.GetLength(0);i++)
+            {
+                for(var j =0; j <inputSignals.GetLength(1);j++)
+                {
+                    inputSignals[i, j] = inputs[i][j];
+                }
+            }
+            var topology = new Topology(outputs.Count, 1, 1, outputs.Count /2);
+            var neuralNetwork = new NeuralNetwork(topology);
+            var difference = neuralNetwork.Learn(outputs.ToArray(), inputSignals, 10);
+
+            var results = new List<double>();
+            for (int i = 0; i < outputs.Count; i++)
+            {
+                var res = neuralNetwork.Predict(inputs[i]).Output;
+                results.Add(res);
+            }
+            for (int i = 0; i < results.Count; i++)
+            {
+                var expected = Math.Round(outputs[i], 2);
+                var actual = Math.Round(results[i], 2);
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
     }
 }
